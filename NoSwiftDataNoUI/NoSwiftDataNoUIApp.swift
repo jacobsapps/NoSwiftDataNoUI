74011f499938a68a09224308962a9c3ee0ff7209
Jacob Bartlett <jacobbartlett95@gmail.com>
2024-04-03 06:35:04 +0100

    Remove nested repo

---
+++ NoSwiftDataNoUI/NoSwiftDataNoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSwiftDataNoUIApp.swift
+//  NoSwiftDataNoUI
+//
+//  Created by Jacob Bartlett on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NoSwiftDataNoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
